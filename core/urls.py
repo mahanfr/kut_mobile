32c4b376504b3bcef4c4d5694ead108c1a321fdd
mahanfarzaneh2000 <mahan.farzaneh2@gmail.com>
2021-08-28 23:18:44 +0430

    urls for each api added

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/announcement/',include('announcement.urls'))
+    path('api/v1/announcement/',include('announcement.urls')),
+    path('api/v1/news/',include('news.urls')),
+    path('api/v1/event/',include('event.urls')),
 ]
